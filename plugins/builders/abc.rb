4fc084c6eb2f70b81d86c67a67ed3ffb12ee41a3
Jamie Macey <jamie.git@tracefunc.com>
2022-09-01 06:01:52 +0000

    Clean up some paths

--- plugins/builders/abc.rb
+++ plugins/builders/abc.rb
@@ -2,10 +2,15 @@ class Builders::Abc < SiteBuilder
   def build
     Dir['src/_songs/*.abc'].each do |song|
       content = File.read(song)
-      title = content.match(/T:(.*)/)[1]
-      add_resource :songs, "#{title.downcase.gsub(" ","_")}.md" do
+      match = content.match(/T:(.*)/)
+      next unless match
+      title = match[1]
+      filename = title.downcase.gsub(/[^a-zA-Z0-9]+/, "_")
+
+      add_resource :songs, "#{filename}.md" do
         title title
         layout :abc
+        permalink "/#{filename}.html"
         content "<pre>\n#{content}\n</pre>\n"
       end
     end
