fec9d692df3a3c7593aff4ed96c7b0cd0503cc05
Jamie Macey <jamie.git@tracefunc.com>
2025-01-04 21:58:13 -0800

    JS autoformat, thanks Zed

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,44 +1,44 @@
-import "index.css"
+import "index.css";
 
 // Import all JavaScript & CSS files from src/_components
-import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
+import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}";
 
 // Yarn
-import abcjs from "abcjs"
-import 'abcjs/abcjs-audio.css'
+import abcjs from "abcjs";
+import "abcjs/abcjs-audio.css";
 
 // Render Music
-Array.from(document.getElementsByClassName('tune')).forEach(function (tune) {
-    console.log(tune)
-    let paperId = tune.id + "-paper"
-    let audioSelector = "#" + tune.id + "-audio"
+Array.from(document.getElementsByClassName("tune")).forEach(function (tune) {
+  console.log(tune);
+  let paperId = tune.id + "-paper";
+  let audioSelector = "#" + tune.id + "-audio";
 
-    let abcString = tune.getElementsByClassName("paper")[0].innerText
-    let visualOptions = { responsive: 'resize' }
-    let visualObj = abcjs.renderAbc(paperId, abcString, visualOptions)
+  let abcString = tune.getElementsByClassName("paper")[0].innerText;
+  let visualOptions = { responsive: "resize" };
+  let visualObj = abcjs.renderAbc(paperId, abcString, visualOptions);
 
-    if (abcjs.synth.supportsAudio()) {
-        let visualOptions = {
-            displayRestart: true,
-            displayPlay: true,
-            displayProgress: true,
-            displayClock: true
-        }
-        let synthControl = new abcjs.synth.SynthController();
-        synthControl.load(audioSelector, null, visualOptions);
-        synthControl.disable(true);
+  if (abcjs.synth.supportsAudio()) {
+    let visualOptions = {
+      displayRestart: true,
+      displayPlay: true,
+      displayProgress: true,
+      displayClock: true,
+    };
+    let synthControl = new abcjs.synth.SynthController();
+    synthControl.load(audioSelector, null, visualOptions);
+    synthControl.disable(true);
 
-        let synth = new abcjs.synth.CreateSynth();
-        let synthOptions = {
-            visualObj: visualObj[0],
-            options: {}
-        }
-        synth.init((synthOptions)).then(function () {
-            synthControl.setTune(visualObj[0], true).then(function (response) {
-                tune.querySelector(".abcjs-inline-audio").classList.remove("disabled");
-            })
-        })
-    }
-})
+    let synth = new abcjs.synth.CreateSynth();
+    let synthOptions = {
+      visualObj: visualObj[0],
+      options: {},
+    };
+    synth.init(synthOptions).then(function () {
+      synthControl.setTune(visualObj[0], true).then(function (response) {
+        tune.querySelector(".abcjs-inline-audio").classList.remove("disabled");
+      });
+    });
+  }
+});
 
-console.info("Bridgetown is loaded!")
+console.info("Bridgetown is loaded!");
