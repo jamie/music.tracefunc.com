e625b8c3fdc4b594e13cdc99cb0541117fe98c7f
Jamie Macey <jamie.git@tracefunc.com>
2026-06-06 16:20:55 -0700

    Clean up matchers for parsing flex

--- plugins/builders/abc.rb
+++ plugins/builders/abc.rb
@@ -4,7 +4,7 @@ class Builders::Abc < SiteBuilder
       content = File.read(song)
 
       title = content.match(/^T:(.*)/)&.captures&.first&.gsub(/\(.*/, "")&.strip
-      book = content.match(/^B:(.*)/)&.captures&.first&.strip.then { |text| "\"#{text}\"" unless text.empty? }
+      book = content.match(/^B:(.*)/)&.captures&.first&.strip.then { |text| "\"#{text}\"" unless text.blank? }
       next unless title || book
       page_name = book || title
       filename = page_name.downcase.gsub(/[^a-zA-Z0-9]+/, "_")
@@ -15,7 +15,7 @@ class Builders::Abc < SiteBuilder
         permalink "/#{filename}.html"
 
         content(content.split(/(?=X:)/).map{|content_block|
-          id = content_block.match(/X:(.*)/)[1]
+          id = content_block.match(/X:(.*)/)[1].strip
           <<~ABC
             <div class="tune" id="tune#{id}">
               <div class="paper" id="tune#{id}-paper">
