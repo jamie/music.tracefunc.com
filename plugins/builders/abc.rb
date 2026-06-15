b955e2aeb1d968a6eacdaf1830a5be12e75e438d
Jamie Macey <jamie.git@tracefunc.com>
2026-06-14 18:03:28 -0700

    Update song collection builder

--- plugins/builders/abc.rb
+++ plugins/builders/abc.rb
@@ -1,35 +1,82 @@
 class Builders::Abc < SiteBuilder
   def build
-    Dir['src/_songs/**/*.abc'].each do |song|
-      content = File.read(song)
+    songs_by_book = Hash.new { |h, k| h[k] = [] }
+    songs_by_game = Hash.new { |h, k| h[k] = [] }
 
-      title = content.match(/^T:(.*)/)&.captures&.first&.gsub(/\(.*/, "")&.strip
-      book = content.match(/^B:(.*)/)&.captures&.first&.strip.then { |text| "\"#{text}\"" unless text.blank? }
-      next unless title || book
-      page_name = book || title
-      filename = page_name.downcase.gsub(/[^a-zA-Z0-9]+/, "_")
-      folder = File.dirname(song).split("/").last
-      folder = nil if folder == "_songs"
+    Dir['src/_songs/**/*.abc'].sort.each do |file|
+      content = File.read(file)
 
-      add_resource :songs, "#{filename}.md" do
-        title page_name
-        layout :abc
-        folder folder
-        permalink ["", "songs", folder, "#{filename}.html"].compact.join("/")
+      content.split(/(?=^X:)/).each do |tune_block|
+        tune_block = tune_block.strip
+        next unless tune_block.start_with?('X:')
 
-        content(content.split(/(?=X:)/).map{|content_block|
-          id = content_block.match(/X:(.*)/)[1].strip
-          <<~ABC
-            <div class="tune" id="tune#{id}">
-              <div class="paper" id="tune#{id}-paper">
-              </div>
-            </div>
-            <pre class="tune-source" id="tune#{id}-source">
-            #{content_block}
-            </pre>
-          ABC
-        }.join("<hr/>\n"))
+        tune_title = parse_field(tune_block, 'T')
+        tune_book  = parse_field(tune_block, 'B')
+        tune_game  = parse_field(tune_block, 'G')
+        next unless tune_title
+
+        tune_slug     = slugify(tune_title)
+        book_slug     = tune_book && slugify(tune_book)
+        game_slug     = tune_game && slugify(tune_game)
+        tune_book_url = tune_book && "/books/#{book_slug}/"
+        tune_game_url = tune_game && "/games/#{game_slug}/"
+
+        song_meta = { "title" => tune_title, "book" => tune_book, "game" => tune_game, "slug" => tune_slug }
+        songs_by_book[tune_book] << song_meta if tune_book
+        songs_by_game[tune_game] << song_meta if tune_game
+
+        tune_content = <<~HTML
+          <div class="tune" id="tune1">
+            <div class="paper" id="tune1-paper"></div>
+          </div>
+          <pre class="tune-source" id="tune1-source">
+          #{tune_block}
+          </pre>
+        HTML
+
+        add_resource :songs, "#{tune_slug}.md" do
+          title tune_title
+          book tune_book
+          game tune_game
+          book_url tune_book_url
+          game_url tune_game_url
+          layout :abc
+          permalink "/songs/#{tune_slug}/"
+          content tune_content
+        end
       end
     end
+
+    songs_by_book.each do |book_name, book_songs|
+      bslug = slugify(book_name)
+      add_resource :books, "#{bslug}.md" do
+        title book_name
+        songs book_songs
+        layout :book_index
+        permalink "/books/#{bslug}/"
+        content ""
+      end
+    end
+
+    songs_by_game.each do |game_name, game_songs|
+      gslug = slugify(game_name)
+      add_resource :games, "#{gslug}.md" do
+        title game_name
+        songs game_songs
+        layout :game_index
+        permalink "/games/#{gslug}/"
+        content ""
+      end
+    end
+  end
+
+  private
+
+  def parse_field(text, letter)
+    text.match(/^#{letter}:(.*)/)&.captures&.first&.strip.then { |v| v&.empty? ? nil : v }
+  end
+
+  def slugify(str)
+    str.downcase.gsub(/[^a-z0-9]+/, '-').gsub(/^-+|-+$/, '')
   end
 end
--- src/index.md
+++ src/index.md
@@ -1,19 +1,44 @@
 ---
-# Feel free to add content and custom Front Matter to this file.
-song_folder:
 layout: default
+title: Home
 ---
 
-<h2><a href="/">Home</a></h2>
+## Books
 
-<h2>Songs</h2>
-
-<ul class="list-group">
-  {% for song in collections.songs.resources %}
-    {% if song.data.folder == page.data.song_folder %}
-      <li class="list-group-item">
-      <a href="{{ song.relative_url }}">{{ song.data.title }}</a>
-      </li>
-    {% endif %}
+<ul class="list-group mb-4">
+  {% for book in collections.books.resources %}
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <a href="{{ book.relative_url }}">{{ book.data.title }}</a>
+      <span class="badge bg-secondary rounded-pill">{{ book.data.songs.size }}</span>
+    </li>
   {% endfor %}
 </ul>
+
+## Games
+
+<ul class="list-group mb-4">
+  {% for game in collections.games.resources %}
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <a href="{{ game.relative_url }}">{{ game.data.title }}</a>
+      <span class="badge bg-secondary rounded-pill">{{ game.data.songs.size }}</span>
+    </li>
+  {% endfor %}
+</ul>
+
+{% assign other_songs = "" | split: "" %}
+{% for song in collections.songs.resources %}
+  {% unless song.data.book or song.data.game %}
+    {% assign other_songs = other_songs | push: song %}
+  {% endunless %}
+{% endfor %}
+{% if other_songs.size > 0 %}
+## Other Songs
+
+<ul class="list-group">
+  {% for song in other_songs %}
+    <li class="list-group-item">
+      <a href="{{ song.relative_url }}">{{ song.data.title }}</a>
+    </li>
+  {% endfor %}
+</ul>
+{% endif %}
