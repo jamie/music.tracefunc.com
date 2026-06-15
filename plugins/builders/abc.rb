68be02ad01bef40be8c6d25b31d0ba0f8f6cca8b
Jamie Macey <jamie.git@tracefunc.com>
2026-06-14 19:14:19 -0700

    Clean up URLs, sort by abc song id

--- plugins/builders/abc.rb
+++ plugins/builders/abc.rb
@@ -13,15 +13,20 @@ class Builders::Abc < SiteBuilder
         tune_title = parse_field(tune_block, 'T')
         tune_book  = parse_field(tune_block, 'B')
         tune_game  = parse_field(tune_block, 'G')
+        tune_x     = tune_block.match(/^X:\s*(\d+)/)&.captures&.first&.to_i || 0
         next unless tune_title
 
-        tune_slug     = slugify(tune_title)
-        book_slug     = tune_book && slugify(tune_book)
-        game_slug     = tune_game && slugify(tune_game)
-        tune_book_url = tune_book && "/books/#{book_slug}/"
-        tune_game_url = tune_game && "/games/#{game_slug}/"
+        tune_slug  = slugify(tune_title)
+        book_slug  = tune_book && slugify(tune_book)
+        game_slug  = tune_game && slugify(tune_game)
+        song_url   = tune_book ? "/#{book_slug}/#{tune_slug}/" : "/songs/#{tune_slug}/"
+        book_url   = tune_book && "/#{book_slug}/"
+        game_url   = tune_game && "/sources/#{game_slug}/"
 
-        song_meta = { "title" => tune_title, "book" => tune_book, "game" => tune_game, "slug" => tune_slug }
+        song_meta = {
+          "title" => tune_title, "book" => tune_book, "game" => tune_game,
+          "slug" => tune_slug, "url" => song_url, "x" => tune_x
+        }
         songs_by_book[tune_book] << song_meta if tune_book
         songs_by_game[tune_game] << song_meta if tune_game
 
@@ -38,10 +43,10 @@ class Builders::Abc < SiteBuilder
           title tune_title
           book tune_book
           game tune_game
-          book_url tune_book_url
-          game_url tune_game_url
+          book_url book_url
+          game_url game_url
           layout :abc
-          permalink "/songs/#{tune_slug}/"
+          permalink song_url
           content tune_content
         end
       end
@@ -49,22 +54,24 @@ class Builders::Abc < SiteBuilder
 
     songs_by_book.each do |book_name, book_songs|
       bslug = slugify(book_name)
+      sorted = book_songs.sort_by { |s| [s["x"], s["title"]] }
       add_resource :books, "#{bslug}.md" do
         title book_name
-        songs book_songs
+        songs sorted
         layout :book_index
-        permalink "/books/#{bslug}/"
+        permalink "/#{bslug}/"
         content ""
       end
     end
 
     songs_by_game.each do |game_name, game_songs|
       gslug = slugify(game_name)
+      sorted = game_songs.sort_by { |s| [s["x"], s["title"]] }
       add_resource :games, "#{gslug}.md" do
         title game_name
-        songs game_songs
+        songs sorted
         layout :game_index
-        permalink "/games/#{gslug}/"
+        permalink "/sources/#{gslug}/"
         content ""
       end
     end
