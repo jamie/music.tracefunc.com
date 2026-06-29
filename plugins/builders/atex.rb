a5491fa1a1b87c8d99b6f91f3cba0677c29546f7
Jamie Macey <jamie.git@tracefunc.com>
2026-06-28 21:38:49 -0700

    Wrap atex song render in default layout

--- plugins/builders/atex.rb
+++ plugins/builders/atex.rb
@@ -3,21 +3,28 @@ class Builders::Atex < SiteBuilder
     Dir['src/_songs/**/*.atex'].sort.each do |file|
       content = File.read(file)
 
-      tune_title    = parse_field(content, 'title')
-      tune_subtitle = parse_field(content, 'subtitle')
+      tune_title = parse_field(content, 'title')
+      tune_game  = parse_field(content, 'subtitle')
+      tune_book  = parse_field(content, 'album')
       next unless tune_title
 
-      tune_slug    = slugify(tune_title)
-      song_url     = "/atex/#{tune_slug}/"
-      file_content = content
+      tune_slug  = slugify(tune_title)
+      book_slug  = tune_book && slugify(tune_book)
+      game_slug  = tune_game && slugify(tune_game)
+      song_url   = tune_book ? "/#{book_slug}/#{tune_slug}/" : "/songs/#{tune_slug}/"
+      book_url   = tune_book && "/#{book_slug}/"
+      game_url   = tune_game && "/sources/#{game_slug}/"
 
-      add_resource :atex_songs, "#{tune_slug}.md" do
+      add_resource :songs, "#{tune_slug}.html" do
         title tune_title
-        subtitle tune_subtitle
-        atex_content file_content
+        book tune_book
+        game tune_game
+        book_url book_url
+        game_url game_url
         layout :atex
         permalink song_url
-        content ""
+        atex content
+        content content
       end
     end
   end
